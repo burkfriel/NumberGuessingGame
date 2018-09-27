f5004a0afde0009608be1398a038e3eeeb5c11c3
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-09-27 13:40:59 -0400

    Finished the code

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -11,17 +11,35 @@ public class NumberGuessingGame {
 
         keyboard = new Scanner(System.in);
         pickedNumber = (int)(Math.random()*100);
+        youGotIt = false;
 
         System.out.println("Hello this is a number guessing game.");
+        System.out.println("The rules are really simple.");
+        System.out.println("*Guess a number if it is too high it will say too high guess again.");
+        System.out.println("*Guess a number if it is too low it will say too low guess again.");
         System.out.println("So guess a number.");
         guessedNumber = keyboard.nextInt();
 
-        while(youGotIt){
+        while(!youGotIt){
 
             if (guessedNumber < pickedNumber){
-                
+                System.out.println("Your Number is too low.");
+
+            } else if (guessedNumber > pickedNumber){
+                System.out.println("Your Number is too high.");
+
+            } else if (guessedNumber == pickedNumber){
+                System.out.println("You got it right.");
+
+                youGotIt = true;
+                break;
             }
+
+            System.out.println("Guess again.");
+            guessedNumber = keyboard.nextInt();
         }
+
+        System.out.println("Thank you for playing my number guessing game.");
 
 
 
